7f653b65f584e5e4181602dcb5ad68bfbeaaa5ce
Dj Aldrinho <aldray.devsoft@gmail.com>
2020-11-25 18:34:00 -0500

    Implementacion de login y registro

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'Auth\LoginController@showLoginForm');
+Route::get('/home', 'HomeController@index')->name('home');
+
+/*
+ * Authentication Routes
+ */
+Auth::routes();
